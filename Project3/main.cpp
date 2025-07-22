7cc0ed4c12475d6cb553918f9db0db74b3d07407
dooyeun <ghkdendus330@gmail.com>
2025-07-22 15:18:52 +0900

    getSumSum 함수 추가

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,10 +1,7 @@
 #include "gmock/gmock.h"
 class Cal {
-<<<<<<< HEAD
-=======
 public:
->>>>>>> bc3dd0a (getSumSum 함수 추가)
-	int getSumSum(int a, int b, int c) {
+	double getSumSum(double a, double b, double c) {
 		return a + b + c;
 	}
 };
